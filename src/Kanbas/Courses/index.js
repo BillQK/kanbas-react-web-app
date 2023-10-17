e5b2ed1b12bdbac61f96cc67096adff06373069c
BillQK <billngo47@gmail.com>
2023-10-17 18:40:56 -0400

    Assignment 3

---
+++ src/Kanbas/Courses/index.js
@@ -0,0 +1,41 @@
+import db from "../../Kanbas/Database";
+import { Navigate, Route, Routes, useParams } from "react-router-dom";
+import CourseNavigation from "./CourseNavigation";
+import Modules from "./Modules";
+import Home from "./Home";
+import AssignmentEditor from "./Assignments/AssignmentEditors";
+import Assignment from "./Assignments";
+import Grades from "./Grades/Index";
+function Courses() {
+  const { courseId } = useParams();
+  const course = db.courses.find((course) => course._id === courseId);
+  return (
+    <div>
+      <h1>Course {course.name}</h1>
+      <CourseNavigation />
+      <div>
+        <div
+          className="overflow-y-scroll position-fixed bottom-0 end-0"
+          style={{
+            left: "320px",
+            top: "50px",
+          }}
+        >
+          <Routes>
+            <Route path="/" element={<Navigate to="Home" />} />
+            <Route path="Home" element={<Home />} />
+            <Route path="Modules" element={<Modules />} />
+
+            <Route path="Assignments" element={<Assignment />} />
+            <Route
+              path="Assignments/:assignmentId"
+              element={<AssignmentEditor />}
+            />
+            <Route path="Grades" element={<Grades />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Courses;
--- src/Kanbas/Database/index.js
+++ src/Kanbas/Database/index.js
@@ -1,3 +1,8 @@
 import courses from "./courses.json";
-const database = { courses };
-export default database;
+import modules from "./modules.json";
+import assignments from "./assignments.json";
+import enrollments from "./enrollments.json";
+import users from "./users.json";
+import grades from "./grades.json";
+const db = { courses, modules, assignments, enrollments, users, grades };
+export default db;
